243cb9abcfef8b728c0afec9a2d0ff7a553fd3a2
Ayush-07-Mishra <ayushmishra312004@gmail.com>
2024-04-28 11:02:31 +0530

    c

--- p8.cpp
+++ p8.cpp
@@ -1,57 +1,58 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
+#include<vector>
 
-#define INF 1000000000
 #define MAX_NODES 100
+#define INF 1000000000
 
 using namespace std;
 
-void floydWarshall(vector<vector<int> >& graph ,int n){
-    vector<vector<int> > dist = graph;
+void floydWarshall(vector<vector<int> >& graph,int n){
+    vector<vector<int> >dist = graph;
 
-    for(int k =0;k<n;k++){
+    for(int k= 0;k<n;k++){
         for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(dist[i][k] != INF && dist[k][j != INF && (dist[i][j] > dist[i][k] + dist[k][j])]){
+            for(int j =0;j<n;j++){
+                if(dist[i][j] > dist[i][k] + dist[k][j] && dist[i][k] != INF && dist[k][j] != INF){
                     dist[i][j] = dist[i][k] + dist[k][j];
                 }
             }
         }
     }
-    // check for the negative cycles
-    for(int i=0;i<n;i++){
-        if(disit[i][i] < 0){
-            dist[i][i] =0;
+    
+    for(int i =0;i<n;i++){
+        if(dist[i][i] < 0){
+            dist[i][i] = 0;
         }
     }
 
     for(int i =0;i<n;i++){
-        for(int j =0;j<m;j++){
+        for(int j =0;j<n;j++){
             if(dist[i][j] == INF){
-                cout<< "INF" ;
+                cout << "INF";
             }else{
                 cout << dist[i][j] << " ";
             }
         }
         cout << endl;
     }
-    
 }
 
 int main(){
-    int n, m;
-    cout << "enter the value of n and m";
-    vector<vector<int> > graph(MAX_NODES,vector<int>(MAX_NODES,INF));
-
-    cout << "enter the start(u) and the end (v) vertex along with capacity(w) :";
-
-    for(int i =0;i<n;i++){
+    int n ,m;
+    cout << "Please enter the value of n and m:\n";
+    cin >> n >> m;
+    vector<vector<int> >graph(MAX_NODES,vector<int>(MAX_NODES, INF));
+    cout << "Enter the start(u) and end(v) vertex along with capacity(w):\n";
+    for(int i =0;i<m;i++){
         int u,v,w;
         cin >> u >> v >> w;
         graph[u][v] = w;
     }
 
-    cout<< "the n*n matrix is :\n";
-    floydWarshall(graph,n); 
+    cout << "The n*n matrix is :\n";
+    floydWarshall(graph,n);
+
+    return 0;
+    
 }
