548477f99af405621aae245d282f05fafaf9b978
Ayush-07-Mishra <ayushmishra312004@gmail.com>
2024-04-30 11:03:14 +0530

    /

--- edmondKArp_prac.cpp
+++ edmondKArp_prac.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
-#include <queue>
+#include<iostream>
+#include<queue>
 using namespace std;
 
-#define n 5
+
+# define n 5
+
 struct node{
     int id,status,pred;
 };
@@ -13,24 +15,24 @@ int res[n][n] = {{0,12,15,0,0},
                  {0,0,0,0,5},
                  {0,0,0,0,0}};
 
-bool bfs(node vert[],node source,node sink)
-{
+bool bfs(node vert[],node source,node sink){
     node u;
-    int i,j;
-    queue <node>q;
-    for(i=0;i<n;i++)
-    {
+    int i ,j;
+    queue<node> q;
+    for(i =0;i<n;i++){
         vert[i].status = 0;
     }
     vert[source.id].pred = -1;
     vert[source.id].status = 1;
+
     q.push(source);
 
     while(!q.empty()){
-        u=q.front();
+        u = q.front();
         q.pop();
-        for(int i=0;i<n;i++){
-            if(res[u.id][i]!=0 && vert[i].status == 0){
+
+        for(int i =0;i<n;i++){
+            if(res[u.id][i] != 0 && vert[i].status == 0){
                 q.push(vert[i]);
                 vert[i].status = 1;
                 vert[i].pred = u.id;
@@ -43,29 +45,32 @@ bool bfs(node vert[],node source,node sink)
 int main(){
     int maxflow = 0;
     node vert[n],source,sink;
-    for(int i=0 ; i<n ; i++)
-        vert[i].id = i ; 
+    for(int i =0;i<n;i++){
+        vert[i].id = i;
+    }
     source.id = 0;
     sink.id = n-1;
+
     int u,v;
-    while( bfs(vert,source,sink)){
-        int augflow = 99999;
-        cout << endl <<"In path :";
-        for(v=sink.id ; v != source.id ; v = vert[v].pred){
-            cout << v <<" -> ";
+
+    while(bfs(vert,source,sink)){
+        int augflow = 1000000000;
+        cout << endl << "In path :";
+        for(int v = sink.id;v !=source.id;v = vert[v].pred){
+            cout<<v<<"->";
             u = vert[v].pred;
-            if(res[u][v] < augflow)
-            {
+            if(res[u][v] < augflow){
                 augflow = res[u][v];
             }
+
         }
         cout << source.id;
-        for(v = sink.id ; v != source.id ; v = vert[v].pred)
-        {
-            u=vert[v].pred;
+        for(v = sink.id; v != source.id ; v = vert[v].pred) {
+            u = vert[v].pred;
             res[u][v] -= augflow;
             res[v][u] += augflow;
         }
+
         maxflow += augflow;
     }
     cout<<endl<<"Maxflow is: "<< maxflow;
