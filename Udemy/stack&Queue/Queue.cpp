6b9913936f70582a4c7a9adb3f344f9a16dcab32
Ayush-07-Mishra <ayushmishra312004@gmail.com>
2024-06-19 12:18:36 +0530

    new codes

--- Udemy/stack&Queue/Queue.cpp
+++ Udemy/stack&Queue/Queue.cpp
@@ -16,18 +16,45 @@ class Node{
 class Queue{
     private:
         Node* first;
-        Node* Last;
+        Node* last;
         int length;
 
     public:
         Queue(int value){
             Node* newNode = new Node(value);
             first = newNode;
-            Last = newNode;
+            last = newNode;
             length = 1;
+        }
+
+        void printQueue(){
+            Node* temp = first;
+            while(temp){
+                cout << temp->value << endl;
+                temp = temp->next;
+            }
+        }
+
+        void getLength(){
+            cout << "Length : " << length <<endl;
+        }
+        void getFirst(){
+            cout << "First : " << first->value << endl;
+        }
+        void getLast(){
+            cout << "Last : " << last->value << endl;
+        }
+
+        void Enqueue(int value){
+            Node* newNode = new Node(value);
+            
         }
 
 };
 int main(){
-    
+    Queue* myQ = new Queue(7);
+    myQ->getFirst();
+    myQ->getLast();
+    myQ->getLength();
+    myQ->printQueue();
 }
