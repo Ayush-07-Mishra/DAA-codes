55205c4d5454da81f3b3344fa610508c288f45e0
Ayush-07-Mishra <ayushmishra312004@gmail.com>
2024-06-25 11:27:54 +0530

    bst

---
+++ Udemy/trees/bst.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node* left;
+        Node* right;
+
+    Node(int value){
+        this->value = value;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    class
+
+}
+
+
+
+int main(){
+    
+}
